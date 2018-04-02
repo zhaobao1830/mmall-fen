c9f51a0979156a53c31c1a6038a8ea94b50a2f5a
赵宝 <zhaobao@lubanec.com>
2018-04-02 17:23:17 +0800

    通用导航开发  footer开发

---
+++ src/service/user-service.js
@@ -0,0 +1,26 @@
+/**
+ * Created by zb on 2018/4/2.
+ */
+var _mm = require('util/mm.js');
+
+var _user = {
+  // 退出
+  logout : function (resolve, reject) {
+    _mm.request({
+      url: _mm.getServerUrl('/user/logout.do'),
+      method: 'POST',
+      success: resolve,
+      error: reject
+    });
+  },
+  // 检查登录状态
+  checkLogin : function(resolve, reject){
+    _mm.request({
+      url     : _mm.getServerUrl('/user/get_user_info.do'),
+      method  : 'POST',
+      success : resolve,
+      error   : reject
+    });
+  },
+}
+exports.module = _user;
--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,22 @@ var config = {
     */
     entry: {
         'common'            : './src/page/common/index.js',
-        'index'             : './src/page/index/index.js'
+        'index'             : './src/page/index/index.js',
+      // 'list'              : './src/page/list/index.js',
+      // 'detail'            : './src/page/detail/index.js',
+      // 'cart'              : './src/page/cart/index.js',
+      // 'order-confirm'     : './src/page/order-confirm/index.js',
+      // 'order-list'        : './src/page/order-list/index.js',
+      // 'order-detail'      : './src/page/order-detail/index.js',
+      // 'payment'           : './src/page/payment/index.js',
+      // 'user-login'        : './src/page/user-login/index.js',
+      // 'user-register'     : './src/page/user-register/index.js',
+      // 'user-pass-reset'   : './src/page/user-pass-reset/index.js',
+      // 'user-center'       : './src/page/user-center/index.js',
+      // 'user-center-update': './src/page/user-center-update/index.js',
+      // 'user-pass-update'  : './src/page/user-pass-update/index.js',
+      // 'result'            : './src/page/result/index.js',
+      // 'about'             : './src/page/about/index.js',
     },
     output: {
         /*
@@ -48,7 +63,7 @@ var config = {
     },
     // 引用一个库，又不想让webpack打包，就用externals
     externals : {
-        'jquery' : 'window.jQuery'
+        'jQuery' : 'window.jQuery'
     },
     module: {
         /*
